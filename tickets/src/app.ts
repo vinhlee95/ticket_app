f1b16f7e1c799223ccdb287d405067a25117a9e4
Vinh Le <lethanhvinh95@gmail.com>
2022-02-17 07:21:12 +0200

    more tickets update

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@shotcode/ticketdevcommon'
+import { errorHandler, NotFoundError, currentUser } from '@cygnetops/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,8 +17,14 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
-app.all('*', async (req: express.Request, res: express.Response) => {
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
